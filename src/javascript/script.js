ebb13f5b1661a9f8ce720f61ed5ba46586fa9086
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2024-08-31 16:31:04 -0300

    implantados home inclusive css

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+    
     $('#mobile_btn').on('click', function(){
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     })
+    
 });
 
 
